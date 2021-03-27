e38633b530eb0c790322e7cf55331ec3e729a2b4
Arjan <69347644+skarjan@users.noreply.github.com>
2021-03-27 02:02:31 +0100

    Working version
    Everything is able to be validated, spare possible missed cases. Which, admittedly, might be quite a few.  Auto fill demonstrates POC.

--- js/main.js
+++ js/main.js
@@ -9,8 +9,8 @@ const _FIRST_NAME_PATTERN = /^[a-z ,.'-]+$/i;
 const _LAST_NAME_PATTERN = /^[a-z ,.'-]+$/i;
 const _MOBILE_NUMBER_PATTERN = /^[0]6\d{8}$|^[\+]316\d{8}$/i;
 const _LAND_LINE_PATTERN = /^0[1-6]\d{1,2}\d{7}|^\+31[1-6]\d{1,2}\d{7}/i;
-// final regex boss
 const _EMAIL_PATTERN = /(?:[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/i;
+const _DOB_PATTERN = /^[\d]{2}-[\d]{2}-[\d]{4}/i;
 
 function setUpValidation() {
 
@@ -53,22 +53,25 @@ function validateAndSetClass(input) {
   if (input.id === "iban") {
     testIbanAndSetClass(input);
   }
-  
+
+  if (input.id === "dateOfBirth") {
+    testInputAndSetClass(input, _DOB_PATTERN);
+  }
 
   return input;
 }
 
-function testIbanAndSetClass(input){
-    
-    if (isValidIBANNumber(input.value)===1){
-        input.classList.remove('is-invalid');
-        input.classList.add('is-valid');
-        trace(`${input.value} is a valid iban!`);
-    } else {
-        input.classList.remove('is-valid');
-        input.classList.add('is-invalid');
-        trace(`${input.value} is an invalid iban!`);
-    }
+function testIbanAndSetClass(input) {
+
+  if (isValidIBANNumber(input.value) === 1) {
+    input.classList.remove('is-invalid');
+    input.classList.add('is-valid');
+    trace(`${input.value} is a valid iban!`);
+  } else {
+    input.classList.remove('is-valid');
+    input.classList.add('is-invalid');
+    trace(`${input.value} is an invalid iban!`);
+  }
 }
 
 function testInputAndSetClass(input, pattern) {
@@ -91,22 +94,46 @@ function addEventListenerToInput(input) {
 }
 
 function addEventListenerToButton(button) {
-    document.getElementById(button).addEventListener('click', function(){
-        ibanFunction();                             
-    })
+  document.getElementById(button).addEventListener('click', function () {
+    autoFillFunction();
+  })
 }
 
 function verifyValidation() {
   let inputs = $(".validate");
   let validInputArray = $(".is-valid");
 
-  if(validInputArray.length == inputs.length){
+  if (validInputArray.length == inputs.length) {
 
-      $("#submitButton").removeAttr("disabled");
-      $(".succes-msg").show();
+    $("#submitButton").removeAttr("disabled");
+    $(".succes-msg").show();
 
-      trace("Validation complete!")
+    trace("Validation complete!")
   }
+}
+
+function autoFillFunction(){
+  // Create a new 'change' event
+var event = new Event('input');
+
+  $(":input")[0].value = "John"
+  $(":input")[0].dispatchEvent(event);
+  $(":input")[1].value = "Doe"
+  $(":input")[1].dispatchEvent(event);
+  $(":input")[2].value = "01-01-1990"
+  $(":input")[2].dispatchEvent(event);
+  $(":input")[3].value = "0612345678"
+  $(":input")[3].dispatchEvent(event);
+  $(":input")[4].value = "0104323354"
+  $(":input")[4].dispatchEvent(event);
+  $(":input")[5].value = "john@doeplanet.eu"
+  $(":input")[5].dispatchEvent(event);
+  $(":input")[6].value = "NL86INGB0002445588"
+  $(":input")[6].dispatchEvent(event);
+
+
+// Dispatch it.
+
 }
 
 
@@ -114,7 +141,7 @@ function verifyValidation() {
 
 function alertValidIBAN(iban) {
 
-    alert(isValidIBANNumber(iban));
+  alert(isValidIBANNumber(iban));
 }
 
 /*
@@ -123,39 +150,109 @@ function alertValidIBAN(iban) {
  * Returns any other number (checksum) when the IBAN is invalid (check digits do not match)
  */
 function isValidIBANNumber(input) {
-    var CODE_LENGTHS = {
-        AD: 24, AE: 23, AT: 20, AZ: 28, BA: 20, BE: 16, BG: 22, BH: 22, BR: 29,
-        CH: 21, CR: 21, CY: 28, CZ: 24, DE: 22, DK: 18, DO: 28, EE: 20, ES: 24,
-        FI: 18, FO: 18, FR: 27, GB: 22, GI: 23, GL: 18, GR: 27, GT: 28, HR: 21,
-        HU: 28, IE: 22, IL: 23, IS: 26, IT: 27, JO: 30, KW: 30, KZ: 20, LB: 28,
-        LI: 21, LT: 20, LU: 20, LV: 21, MC: 27, MD: 24, ME: 22, MK: 19, MR: 27,
-        MT: 31, MU: 30, NL: 18, NO: 15, PK: 24, PL: 28, PS: 29, PT: 25, QA: 29,
-        RO: 24, RS: 22, SA: 24, SE: 24, SI: 19, SK: 24, SM: 27, TN: 24, TR: 26,   
-        AL: 28, BY: 28, CR: 22, EG: 29, GE: 22, IQ: 23, LC: 32, SC: 31, ST: 25,
-        SV: 28, TL: 23, UA: 29, VA: 22, VG: 24, XK: 20
-    };
-    var iban = String(input).toUpperCase().replace(/[^A-Z0-9]/, ''), // keep only alphanumeric characters
-            code = iban.match(/^([A-Z]{2})(\d{2})([A-Z\d]+)$/), // match and capture (1) the country code, (2) the check digits, and (3) the rest
-            digits;
+  var CODE_LENGTHS = {
+    AD: 24,
+    AE: 23,
+    AT: 20,
+    AZ: 28,
+    BA: 20,
+    BE: 16,
+    BG: 22,
+    BH: 22,
+    BR: 29,
+    CH: 21,
+    CR: 21,
+    CY: 28,
+    CZ: 24,
+    DE: 22,
+    DK: 18,
+    DO: 28,
+    EE: 20,
+    ES: 24,
+    FI: 18,
+    FO: 18,
+    FR: 27,
+    GB: 22,
+    GI: 23,
+    GL: 18,
+    GR: 27,
+    GT: 28,
+    HR: 21,
+    HU: 28,
+    IE: 22,
+    IL: 23,
+    IS: 26,
+    IT: 27,
+    JO: 30,
+    KW: 30,
+    KZ: 20,
+    LB: 28,
+    LI: 21,
+    LT: 20,
+    LU: 20,
+    LV: 21,
+    MC: 27,
+    MD: 24,
+    ME: 22,
+    MK: 19,
+    MR: 27,
+    MT: 31,
+    MU: 30,
+    NL: 18,
+    NO: 15,
+    PK: 24,
+    PL: 28,
+    PS: 29,
+    PT: 25,
+    QA: 29,
+    RO: 24,
+    RS: 22,
+    SA: 24,
+    SE: 24,
+    SI: 19,
+    SK: 24,
+    SM: 27,
+    TN: 24,
+    TR: 26,
+    AL: 28,
+    BY: 28,
+    CR: 22,
+    EG: 29,
+    GE: 22,
+    IQ: 23,
+    LC: 32,
+    SC: 31,
+    ST: 25,
+    SV: 28,
+    TL: 23,
+    UA: 29,
+    VA: 22,
+    VG: 24,
+    XK: 20
+  };
+  var iban = String(input).toUpperCase().replace(/[^A-Z0-9]/, ''), // keep only alphanumeric characters
+    code = iban.match(/^([A-Z]{2})(\d{2})([A-Z\d]+)$/), // match and capture (1) the country code, (2) the check digits, and (3) the rest
+    digits;
 
 
-    // check syntax and length
-    if (!code || iban.length !== CODE_LENGTHS[code[1]]) {
-        return false;
-    }
-    // rearrange country code and check digits, and convert chars to ints
-    digits = (code[3] + code[1] + code[2]).replace(/[A-Z]/g, function (letter) {
-        return letter.charCodeAt(0) - 55;
-    });
-    // final check
-    return mod97(digits);
+  // check syntax and length
+  if (!code || iban.length !== CODE_LENGTHS[code[1]]) {
+    return false;
+  }
+  // rearrange country code and check digits, and convert chars to ints
+  digits = (code[3] + code[1] + code[2]).replace(/[A-Z]/g, function (letter) {
+    return letter.charCodeAt(0) - 55;
+  });
+  // final check
+  return mod97(digits);
 }
 
 function mod97(string) {
-    var checksum = string.slice(0, 2), fragment;
-    for (var offset = 2; offset < string.length; offset += 7) {
-        fragment = String(checksum) + string.substring(offset, offset + 7);
-        checksum = parseInt(fragment, 10) % 97;
-    }
-    return checksum;
+  var checksum = string.slice(0, 2),
+    fragment;
+  for (var offset = 2; offset < string.length; offset += 7) {
+    fragment = String(checksum) + string.substring(offset, offset + 7);
+    checksum = parseInt(fragment, 10) % 97;
+  }
+  return checksum;
 }
